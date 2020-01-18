d7c19561f6ca5160d1fb292170c95ea22ca8b9f1
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-01-18 10:11:28 +0530

    Switch string to double quotes
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- Python/Make string of specified format.py
+++ Python/Make string of specified format.py
@@ -3,9 +3,9 @@
 print("Enter z to terminate.\n")
 string = "a"
 choice = ""
-while choice != 'z':
-	choice = input("Which one? 'b' or 'c'? ")
-	if choice == 'a' or choice == 'b':
-		string += choice
-	print("The current string is: " + string)
+while choice != "z":
+    choice = input("Which one? 'b' or 'c'? ")
+    if choice == "a" or choice == "b":
+        string += choice
+    print("The current string is: " + string)
 print("The final string is: " + string)
